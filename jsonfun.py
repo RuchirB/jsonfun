8e46a2a9f39b2322e1079725455c5cdb0a134f99
Ruchir Baronia <ruchirbaronia@gmail.com>
2019-06-07 22:17:49 -0700

    second commit

--- jsonfun.py
+++ jsonfun.py
@@ -1,5 +1,19 @@
 import json
-with open('/Users/ruchirbaronia/Desktop/philippe_messages.json') as f:
-	data = json.load(f)
+#Converting JSON file to a dictioanry object
+with open('/Users/ruchirbaronia/Desktop/JSONfun/philippe_messages.json') as philippesMessage:
+	data = json.load(philippesMessage) 
 
-print(data)
+#Printing the three messages individually
+print (data["message1"]) 
+print (data["message2"])
+print (data["message3"]) 
+
+#Converting back into a JSON file called dataFile.json
+with open ("dataFile.json", "w") as philippesOutput:
+	json.dump(data, philippesOutput, indent=3)
+
+#Converting into a string and reprinting with each child indented 3 spaces
+print(json.dumps(data, indent=3))
+for myString in data:
+	if(data[myString].startswith("I")):
+		print (myString + " starts with an I")
---
+++ parsingNewslensJSON.py
@@ -0,0 +1,23 @@
+import urllib.request, json
+
+#Grabs JSON file from URL
+jsonUrl = urllib.request.urlopen("https://newslens.berkeley.edu/api/lanes/recent2")
+#Referencing index 0 since newLensAPI.json is actuallhy a list of one index that contains the json object
+jsonDictionary = json.load(jsonUrl)[0]
+
+
+#WThis returns a list unless you add "[0]" to the above line. then it returns a dict
+print(type(jsonDictionary))
+
+with open ("/Users/ruchirbaronia/Desktop/JSONfun/newsLensAPI.json", "w") as jsonFile:
+	json.dump(jsonDictionary, jsonFile, indent=3)
+
+
+#All the high level categories
+for subDict in jsonDictionary:
+	print(subDict)
+
+print("_____________________________________TOP HIGHLIGHTS_____________________________________________________")
+
+for newsHighlights in jsonDictionary["latest_highlights"]:
+	print(newsHighlights["title"])
