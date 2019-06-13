b7dfcda7a9ca534b881a84bb8b7afcc1e38b881b
Ruchir Baronia <ruchirbaronia@gmail.com>
2019-06-13 09:15:02 -0700

    logic for 10 events + people

--- newslensScriptJune10.py
+++ newslensScriptJune10.py
@@ -44,8 +44,27 @@ class Helper:
 
 
 		print("The " +storyName + " story started " +(str(daysAgo)) + " hours ago." +" The latest update from this story comes from " +(str(daysAgoLast)) +" hours ago, when " + storySummary)
+		print("\n Do you want me to tell you what people have said or walk you through the last 10 events in the story?")
 
+	@staticmethod
+	def last10Events(storyIndex):
+		print("_____________Here are the last ten events: _________________")
+		storyEvents = Helper.jsonRequest[storyIndex]["latest_highlights"]
+		for x in range (10):
+			print(str(x+1) +") "+storyEvents[x]["summary_title"] + "\n")
 
+	@staticmethod
+	def peopleSaid(storyIndex):
+		highlightInfoUrl = "https://newslens.berkeley.edu/api/highlight_info/" + str(Helper.jsonRequest[storyIndex]["latest_highlights"][0]["_id"] )
+		peopleInfoUrl = "https://newslens.berkeley.edu/api/story/" + str(Helper.jsonRequest[storyIndex]["latest_highlights"][0]["ntopic"])+ "/people"
+		highlightJson = requests.get(highlightInfoUrl).json()
+		peopleList = requests.get(peopleInfoUrl).json()
+
+		#print(peopleList)
+		print(peopleList[0]["name"] +", " +peopleList[1]["name"] + ", and " +peopleList[2]["name"] +" commented on the issue. " + peopleList[2]["name"] 
+			+" said \"" + highlightJson["descriptions"][2]["para"] +"\"")
+
+storyIndex = 0
 
 Helper.init()
 print("Hi, I'm NewsLens!")
@@ -61,8 +80,23 @@ for x in Helper.listOfThree:
 	for individualWord in userInput.split():
 		if individualWord.lower() in x.lower():
 			#print("Found it in " +x +" print " +str(Helper.listOfThree.index(x)))
-			Helper.elaborateOnStory(Helper.listOfThree.index(x))
+			storyIndex=Helper.listOfThree.index(x)
+			Helper.elaborateOnStory(storyIndex)
 			breakLoop=True
 			break
 	if breakLoop is True:
 		break
+
+userInput = input()
+
+if("last" in userInput or "10 events" in userInput):
+	Helper.last10Events(storyIndex)
+elif("people" in userInput or "said" in userInput):
+	Helper.peopleSaid(storyIndex)
+
+userInput = input()
+
+if("last" in userInput or "10 events" in userInput):
+	Helper.last10Events(storyIndex)
+elif("people" in userInput or "said" in userInput):
+	Helper.peopleSaid(storyIndex)
